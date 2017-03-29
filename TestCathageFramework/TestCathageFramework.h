b55c7e5699484696a2644a3752d565fec446689a
Maxim Bolotov <maximbolotov@itomy.ch>
2017-03-29 12:24:59 +0300

    added framework

---
+++ TestCathageFramework/TestCathageFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestCathageFramework.h
+//  TestCathageFramework
+//
+//  Created by Maxim Bolotov on 3/29/17.
+//  Copyright © 2017 Xendit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCathageFramework.
+FOUNDATION_EXPORT double TestCathageFrameworkVersionNumber;
+
+//! Project version string for TestCathageFramework.
+FOUNDATION_EXPORT const unsigned char TestCathageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCathageFramework/PublicHeader.h>
+
+
---
+++ TestCathageFramework/TestFrameworkFile.swift
@@ -0,0 +1,17 @@
+//
+//  TestFrameworkFile.swift
+//  TestCathageFramework
+//
+//  Created by Maxim Bolotov on 3/29/17.
+//  Copyright © 2017 Xendit. All rights reserved.
+//
+
+import Foundation
+
+public class TestFrameworkFile : NSObject {
+    
+    public func testFunction() {
+        print("YEAAAA testFunction !!!!!")
+    }
+    
+}
